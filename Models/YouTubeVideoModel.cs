dbb124ffd943146878ee8ea8806c4680d5612d82
Yunmin20 <aiyunnihaikal20@gmail.com>
2024-10-15 11:27:41 +0800

    SWC4443

---
+++ Models/YouTubeVideoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace YouTubeApiProjectYuni.Models
+{
+    public class YouTubeVideoModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string VideoUrl { get; set; }
+
+    }
+}
